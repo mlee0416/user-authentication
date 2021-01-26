07517216d15271f172e1798a60e799dfebd1c6b5
Michael Lee <mlee0416@gmail.com>
2021-01-25 22:57:36 -0800

    update

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const NewPlace = () => {
-  return (
-    <h1> New Places</h1>
-  )
-}
+  return <h2>NewPlace Works!</h2>
+};
 
 export default NewPlace;
--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,29 +1,30 @@
-import React from "react";
-import UserItem from "./UserItem";
-const UsersList = (props) => {
-	if (props.items.length === 0) {
-		return (
-			<div>
-				<h2>No users found.</h2>
-			</div>
-		);
-	}
+import React from 'react';
 
-	return (
-		<ul>
-			{props.items.map((user) => {
-				return (
-					<UserItem
-						key={user.id}
-						id={user.id}
-						image={user.image}
-            name={user.name}
-            placeCount = {user.places}
-					/>
-				)
-			})}
-		</ul>
-	);
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UsersLists = props => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users founds.</h2>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
 };
 
-export default UsersList;
+export default UsersLists;
